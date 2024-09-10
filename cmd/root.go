c5b4da93b26a9d32cc8a072642fd93d7c4a2f22c
devhindo <lildevhind@gmail.com>
2024-09-10 05:36:25 +0300

    saves svg file but in ico ext

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,19 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -24,7 +27,21 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	RunE: img,
+}
+
+var (
+	query string
+	ext   string
+	size  int
+)
+
+type Img struct {
+	Name string `json:"name"`
+}
+
+type ImageListResponse struct {
+	Icons []string `json:"icons"`
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -45,7 +62,112 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringVarP(&query, "query", "q", "", "image query you want to search (required)")
+	rootCmd.MarkFlagRequired("name")
+
+	rootCmd.Flags().StringVarP(&ext, "extension", "e", "", "extension of the image you want to search (required)")
+	rootCmd.MarkFlagRequired("ext")
+
+	rootCmd.Flags().IntVarP(&size, "size", "s", 0, "size of the image you want to search (optional)")
 }
 
+func img(cmd *cobra.Command, args []string) error {
 
+	apiURL := "https://api.iconify.design"
+
+	apiSearchURL, err := url.Parse(apiURL + "/search")
+	if err != nil {
+		return fmt.Errorf("can't parse url: %w", err)
+	}
+
+	params := url.Values{}
+	params.Add("query", strings.ToLower(query))
+	apiSearchURL.RawQuery = params.Encode()
+
+
+	imgListResp, err := http.Get(apiSearchURL.String())
+	if err != nil {
+		return fmt.Errorf("can't get image list: %w", err)
+	}
+
+	defer imgListResp.Body.Close()
+
+	if imgListResp.StatusCode != http.StatusOK {
+		return fmt.Errorf("can't get image list: server error.  %w", err)
+	}
+
+	imgListBody, err := io.ReadAll(imgListResp.Body)
+	if err != nil {
+		return fmt.Errorf("can't read image list: %w", err)
+	}
+
+	var imgList ImageListResponse
+	err = json.Unmarshal(imgListBody, &imgList)
+	if err != nil {
+		return fmt.Errorf("can't unmarshal image list: %w", err)
+	}
+
+	//	fmt.Println(imgList.Icons)
+
+	icon := ""
+
+	// check if exists a file with the name icon in the current working directory
+	for _, img := range imgList.Icons {
+		tmp := img + "." + ext
+		if _, err := os.Stat(tmp); err == nil {
+			continue
+		}
+
+		icon = img
+		break
+	}
+
+	// split into prefix and name
+	prefix := strings.Split(icon, ":")[0]
+	name := strings.Split(icon, ":")[1]
+	fmt.Println(prefix, name)
+
+	retrieveIconURL := apiURL + "/" + prefix + "/" + name + ".svg"
+
+	iconResp, err := http.Get(retrieveIconURL)
+	if err != nil {
+		return fmt.Errorf("can't get image: %w", err)
+	}
+
+	defer iconResp.Body.Close()
+
+	if iconResp.StatusCode != http.StatusOK {
+		return fmt.Errorf("can't get image: server error.  %w", err)
+	}
+
+	iconBody, err := io.ReadAll(iconResp.Body)
+	if err != nil {
+		return fmt.Errorf("can't read image: %w", err)
+	}
+
+	// create a file with the name icon in the current working directory
+	iconFile, err := os.Create(name + "." + ext)
+	if err != nil {
+		return fmt.Errorf("can't create image file: %w", err)
+	}
+
+	defer iconFile.Close()
+
+	_, err = iconFile.Write(iconBody)
+	if err != nil {
+		return fmt.Errorf("can't write image file: %w", err)
+	}
+
+		fmt.Println(string(iconBody))
+
+	//	fmt.Println("Image downloaded successfully")
+
+	/*
+	   pwd, err := os.Getwd()
+
+	   	if err != nil {
+	   		return fmt.Errorf("can't get current working directory: %w", err)
+	   	}
+	*/
+	return nil
+}
